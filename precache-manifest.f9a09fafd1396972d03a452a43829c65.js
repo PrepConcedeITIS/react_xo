c45ca3248d44285527556066c454252249e0e359
PrepConcedeITIS <tagmirs_letter@mail.ru>
2019-12-24 15:55:57 +0300

    Updates

---
+++ precache-manifest.f9a09fafd1396972d03a452a43829c65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e836a349b47b295f44cd42b8f48f6359",
+    "url": "/react_xo/index.html"
+  },
+  {
+    "revision": "f9ad7ec88c23188795f2",
+    "url": "/react_xo/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "32dd5287ec9008b8403e",
+    "url": "/react_xo/static/js/2.ebc59ef6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_xo/static/js/2.ebc59ef6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f9ad7ec88c23188795f2",
+    "url": "/react_xo/static/js/main.dab831dc.chunk.js"
+  },
+  {
+    "revision": "a291875ac5f9d5aaf280",
+    "url": "/react_xo/static/js/runtime-main.4fa29ab8.js"
+  }
+]);
